7cd00548044796b2c07966e65589fd809eacb3a4
o-kuhiiro <okuhiiro@gmail.com>
2016-12-06 22:19:55 +0900

    minimistテスト

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 const electron = require('electron')
 const app = electron.app
 
-//const minimist = require('minimist');
-
-//var argv = minimist(process.argv);
+const minimist = require('minimist');
+var argv = minimist(process.argv);
+console.log(argv);
 
 app.quit();
