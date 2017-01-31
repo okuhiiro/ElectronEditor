58fa2f26c3170f057cb62e51e47bf1e3a63b652a
o-kuhiiro <okuhiiro@gmail.com>
2017-01-31 14:36:15 +0900

    getConfigFileメソッド追加
    このメソッドでデフォルトの設定ファイルを読み込めるようにするが、読み込む処理は一旦保留

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const electron = require("electron");
+const electron = require('electron');
 const app = electron.app;
 const path = require('path');
+const fs = require('fs');
 // コマンドライン引数パーサ
 const minimist = require('minimist');
 // src/～以下で絶対パスでrequireできるように
@@ -11,21 +12,44 @@ require('app-module-path').addPath(__dirname);
 const paths = require('paths');
 
 // Usage:
-//  node_modules\.bin\electron . --user-data-dir <path> --locale <string>
+//  node_modules\.bin\electron . --user-data-dir <path> --config-file <path>
 // Ex:
-//   node_modules\.bin\electron . --user-data-dir C:\app\ --locale hogehoge
+//   node_modules\.bin\electron . --user-data-dir C:\app\ --config-file C:\app\User\locale.json
 //   npm run start_full_option
 var args = minimist(
     process.argv,
     {string: ['user-data-dir', 'locale']}
 );
 
-var userDataDir = args['user-data-dir']
-if (!userDataDir)
-{
-    userDataDir = paths.getAppDataPath(process.platform);
+function getUserDataDir() {
+    var userDataDir = args['user-data-dir']
+    if (!userDataDir)
+    {
+        userDataDir = paths.getAppDataPath(process.platform);
+    }
+
+    return userDataDir;
 }
 
-console.log(userDataDir);
+function getConfigFile() {
+    var configFile = args['config-file']
+
+    if (!configFile)
+    {
+        var userData = app.getPath('userData');
+        var defaultConfigFile = path.join(userData, 'User', 'locale.json');
+        try {
+            fs.statSync(defaultConfigFile);
+        } catch (e) {
+
+        }
+    }
+
+    return configFile;
+}
+
+//console.log(app.getPath('userData'));
+//console.log(getUserDataDir());
+getConfigFile();
 
 app.quit();
